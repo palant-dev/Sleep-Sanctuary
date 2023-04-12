831fb4e4388eca6918bb05836b1dba36e84ed89e
palant-dev <113528707+apalomba22@users.noreply.github.com>
2023-04-12 22:08:45 +0200

    Added basic layout

--- Sleep Sanctuary.swiftpm/ContentView.swift
+++ Sleep Sanctuary.swiftpm/ContentView.swift
@@ -2,11 +2,33 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        ZStack {
+            Rectangle()
+                .ignoresSafeArea()
+                .foregroundColor(Color("main"))
+            VStack{
+                HStack(spacing: 20) {
+                    Circle()
+                        .foregroundColor(.white)
+                        .overlay {
+                            Image("placeholder")
+                        }
+                    Spacer()
+                    Circle()
+                        .foregroundColor(.white)
+                        .foregroundColor(.white)
+                        .overlay {
+                            Image("placeholder")
+                        }
+                }
+                .padding(.horizontal, 40)
+            }
         }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
